c7977d83691dcf85464717fac9a0afab0c761ce0
Daniel Wehner <dawehner@googlemail.com>
2009-05-12 01:30:15 +0200

    update

--- info-2/testat-1/lambda/Examples.java
+++ info-2/testat-1/lambda/Examples.java
@@ -5,14 +5,19 @@ public class Examples {
   // (ßx.(x x))(ßx. (x x))
 
   ITerm x = new Variable("x");
-  ITerm term1 = new Applikation(
-    new Abstraktion(x, new Applikation(x x)),
-    new Abstraktion(x, new Applikation(x x))
-  );
+  // Funktioniert aus irgendeinem nicht ersichtlichen Grund nicht.
+  // ITerm term1 = new Applikation(new Abstraktion(x, new Applikation(x, x)), new Abstraktion(x, new Applikation(x, x)));
   
-  // (ßx.(ßy.(+ y))) 1 2
+  // (ßx.(ßy.(y))) 1 2
   ITerm y = new Variable("y");
   ITerm one = new GanzeZahl(1);
   ITerm two = new GanzeZahl(2);
-//  ITerm term2 = new 
+  //  ITerm term2 = new Applikation(new Applikation(new Abstraktion(x, new Abstraktion(y, y)), one), two);
+  
+  // (((ßx.(ßy.(x y))) (ßx.(x x))(ßx. (x x))) (ßx.(ßy.(y))) 1 2)
+   ITerm term3 = new Applikation(
+   /** new Applikation(
+      new Abstraktion(x, new Abstraktion(y, new Applikation(x, y))),
+      new Applikation(new Abstraktion(x, new Applikation(x, x)), new Abstraktion(x, new Applikation(x, x)))),
+    new Applikation(new Applikation(new Abstraktion(x, new Abstraktion(y, y)), one), two)); */
 }
